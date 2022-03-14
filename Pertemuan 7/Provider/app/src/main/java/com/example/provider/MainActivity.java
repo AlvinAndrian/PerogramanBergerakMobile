5a8d3428ec3310f07a1c1d616695300c0c170258
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2022-03-14 11:19:37 +0700

    Pertemuan 7

---
+++ Pertemuan 7/Provider/app/src/main/java/com/example/provider/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.provider;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.loader.content.CursorLoader;
+import android.Manifest;
+import android.app.ListActivity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+    final private int REQUEST_READ_CONTACTS = 123;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS);
+        } else{
+            ListContacts();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode
+            , String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ListContacts();
+                } else {
+                    Toast.makeText(MainActivity.this
+                            , "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode
+                        , permissions, grantResults);
+        }
+    }
+    protected void ListContacts(){
+        Uri allContacts = Uri.parse("content://contacts/people");
+        String[] projection = new String[]
+                {ContactsContract.Contacts._ID,
+                        ContactsContract.Contacts.DISPLAY_NAME,
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER};
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(
+                this,
+                allContacts,
+                projection,
+                null,
+                null,
+                null);
+        c = cursorLoader.loadInBackground();
+        String[] columns = new String[]{
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts._ID};
+        int[] views = new int[]{R.id.contactName, R.id.contactID};
+        SimpleCursorAdapter adapter;
+        //---Honeycomb and later---
+        adapter = new SimpleCursorAdapter(
+                this, R.layout.activity_main, c, columns, views,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        this.setListAdapter(adapter);
+    }
+}
+
