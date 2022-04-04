48608e713627fcb78110c80d7fcbfe835f428fc3
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2022-04-04 09:51:53 +0700

    Pertemuan 8

---
+++ Pertemuan 8/AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TEXT_STATE = "currentText";
+    private TextView mTextView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //  Initialize mTextView
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+        // Restore TextView if there is a savedInstanceState
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask (View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+
+        // Start the AsyncTask.
+        // The AsyncTask has a callback that will update the text view.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
